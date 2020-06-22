c862efc3c740a93a8e98c4584dfeb0834a1cfb07
Brian Lego <legobw@gmail.com>
2020-06-22 17:00:04 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -23,3 +23,19 @@ def output_array (array)
     counter +=1 
   end 
 end 
+
+def return_string_array (array)
+  counter = 0 
+  while counter<counter.size do
+    array.[counter]to_s 
+    counter += 1
+  end
+  puts array
+end   
+
+
+
+
+
+
+
