2b975f400ab25ae23097ce45a5f3962c0d361524
Brian Lego <legobw@gmail.com>
2020-06-22 16:56:50 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,25 @@
 # Write your methods here
+
+def loop_message_five_times (message)
+  counter=0 
+  while counter<=5 do
+    puts message 
+    counter += 1 
+  end
+end   
+
+def loop_message_n_times (message, n)
+  counter = 0 
+  while counter<=n do
+    puts message
+    counter +=1 
+  end
+end  
+
+def output_array (array)
+  counter = 0
+  while counter.size do
+    puts array
+    counter +=1 
+  end 
+end 
